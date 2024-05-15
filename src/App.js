b3c3aaf99f2d882147dd2731e6da6f13c03550bf
NostraPredator <hc4sgis@gmail.com>
2024-05-15 14:18:56 +0530

    Added Local Storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TaskList from "./TaskList";
 import TaskForm from "./TaskForm";
 import Navbar from "./Navbar";
@@ -9,14 +9,28 @@ const App = () => {
   const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState("all");
 
+  useEffect(() => {
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, []);
+
   const addTask = (newTask) => {
-    const updatedTasks = [...tasks, newTask];
-    updatedTasks.sort((a, b) => new Date(a.date) - new Date(b.date));
-    setTasks(updatedTasks);
+    setTasks(() => {
+      const updatedTasks = [...tasks, newTask];
+      updatedTasks.sort((a, b) => new Date(a.date) - new Date(b.date));
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
 
   const deleteTask = (taskId) => {
-    setTasks(tasks.filter((task, index) => index !== taskId));
+    setTasks(() => {
+      const updatedTasks = tasks.filter((task, index) => index !== taskId);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+      return updatedTasks;
+    });
   };
 
   return (
